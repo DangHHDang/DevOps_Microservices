743536e70e9e5302a87f6e21ff909cc85cf45d95
dangdhh <dangdang0511gmail.com>
2022-11-18 10:08:29 +0700

    update code

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=dangdang511/devops-microservices-api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy predict-kubernate --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get deploy,rs,svc,pods
 # Step 4:
 # Forward the container port to a host
+kubectl expose pod predict-kubernate-668d5bf96d-lphbb --port=80 --name=api
+kubectl port-forward pod/predict-kubernate-668d5bf96d-lphbb 8080:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=dangdang511/devops-microservices-api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+#docker login -u <YOUR_USER_NAME> --p <YOUR_PASSWORD>
+docker login -u dangdang511 --p Haidang511@
+docker tag api $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
